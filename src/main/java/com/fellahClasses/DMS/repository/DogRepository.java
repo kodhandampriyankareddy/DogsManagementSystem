88e60bc538fceb55839b976484a34d0d6f0e959a
PriyankaReddy <priyanka.anf227@gmail.com>
2023-04-29 23:13:13 -0500

    updated

---
+++ src/main/java/com/fellahClasses/DMS/repository/DogRepository.java
@@ -0,0 +1,19 @@
+package com.fellahClasses.DMS.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.fellahClasses.DMS.Models.Dog;
+import com.fellahClasses.DMS.Models.Trainer;
+
+public interface DogRepository extends CrudRepository<Dog, Integer> {
+	List<Dog> findByName(String name);
+
+	void delete(Dog d);
+
+	Iterable<Dog> findAll();
+
+	Optional<Trainer> findById(int id);
+}
